820115362658bbe419a193034c50931df91fd34c
Sebastian Ramirez <sebas14737@gmail.com>
2026-02-24 08:10:34 -0500

    Guarda cambios con la clase person y su BD

---
+++ src/main/java/co/edu/cesde/ga/app/Main.java
@@ -0,0 +1,4 @@
+package co.edu.cesde.ga.app;
+
+public class Main {
+}
---
+++ src/main/java/co/edu/cesde/ga/model/person.java
@@ -0,0 +1,16 @@
+package co.edu.cesde.ga.model;
+
+import java.util.Date;
+
+public class person {
+long userId;
+String code;
+String documentNumber;
+String firstName;
+String lastName;
+String userName;
+String status;
+
+}
+
+
