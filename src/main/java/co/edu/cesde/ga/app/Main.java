afeaf5f1086a0ec2e7a37481df759cfb031e88a7
Sebastian Ramirez <sebas14737@gmail.com>
2026-02-24 09:45:20 -0500

    se cargan datos de 2 usuarios con constructores y getter y setter

--- src/main/java/co/edu/cesde/ga/app/Main.java
+++ src/main/java/co/edu/cesde/ga/app/Main.java
@@ -1,4 +1,36 @@
 package co.edu.cesde.ga.app;
+import co.edu.cesde.ga.model.person;
 
 public class Main {
+    public static void main(String[] args) {
+
+        //crea una persona con un costructor vacio
+        person person = new person();
+        person.setUserId(1L);
+        person.setCode("123");
+        person.setFirstName("John");
+        person.setLastName("Doe");
+        person.setDocumentNumber("123456");
+        person.setStatus("Active");
+
+        // imprime los datos
+        System.out.println("User Id: " + person.getUserId());
+        System.out.println("Code:  " + person.getCode());
+        System.out.println("FirstName:  " + person.getFirstName());
+        System.out.println("LastName:  " + person.getLastName());
+        System.out.println("DocumentNumber: " + person.getDocumentNumber());
+        System.out.println("Status:  " + person.getStatus());
+
+        // crea una persona con un constructor lleno
+        person person1 = new person(2L, "456", "456789", "Juan", "Lopez", "JuanLP", "Activo");
+        System.out.println("User Id: " + person1.getUserId());
+        System.out.println("Code:  " + person1.getCode());
+        System.out.println("FirstName:  " + person1.getFirstName());
+        System.out.println("LastName:  " + person1.getLastName());
+        System.out.println("DocumentNumber:  " + person1.getDocumentNumber());
+        System.out.println("Status:  " + person1.getStatus());
+    }
+
 }
+
+
--- src/main/java/co/edu/cesde/ga/model/person.java
+++ src/main/java/co/edu/cesde/ga/model/person.java
@@ -3,14 +3,82 @@ package co.edu.cesde.ga.model;
 import java.util.Date;
 
 public class person {
-long userId;
-String code;
-String documentNumber;
-String firstName;
-String lastName;
-String userName;
-String status;
+private long userId;
+private String code;
+private String documentNumber;
+private String firstName;
+private String lastName;
+private String userName;
+private String status;
 
+    public person() {
+    }
+
+    public person(long userId, String code, String documentNumber, String firstName, String lastName, String userName, String status) {
+        this.userId = userId;
+        this.code = code;
+        this.documentNumber = documentNumber;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.status = status;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDocumentNumber() {
+        return documentNumber;
+    }
+
+    public void setDocumentNumber(String documentNumber) {
+        this.documentNumber = documentNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
 
 
